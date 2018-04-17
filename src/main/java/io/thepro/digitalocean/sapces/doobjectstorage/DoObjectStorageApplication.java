d31e0d77ea04d15fd2f815bf26b85269cc49e28d
gladius <gladius.thayalarajan@gmail.com>
2018-04-17 22:53:00 +0530

    first commit

---
+++ src/main/java/io/thepro/digitalocean/sapces/doobjectstorage/DoObjectStorageApplication.java
@@ -0,0 +1,12 @@
+package io.thepro.digitalocean.sapces.doobjectstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoObjectStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoObjectStorageApplication.class, args);
+	}
+}
---
+++ src/test/java/io/thepro/digitalocean/sapces/doobjectstorage/DoObjectStorageApplicationTests.java
@@ -0,0 +1,16 @@
+package io.thepro.digitalocean.sapces.doobjectstorage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DoObjectStorageApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
